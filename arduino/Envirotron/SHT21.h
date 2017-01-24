ddc9e0dbe51e7c9298768970b98599345ee162b8
watson boyett <watson.boyett@apmtinc.com>
2017-01-24 16:25:08 -0600

    Added register definitions for SHT21 and MS5607 chips

---
+++ arduino/Envirotron/SHT21.h
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+
+#define SHT21_ADDR (0x40)
+
+// RH = -6 + 125 * (S_RH / 2^16)
+// T = -46.85 + 175.72 * (S_T / 2^16)
+
+#define SHT21_MEAS_T_HOLD_ADDR    (0xE3)
+#define SHT21_MEAS_RH_HOLD_ADDR   (0xE5)
+#define SHT21_MEAS_T_NOHOLD_ADDR  (0xF3)
+#define SHT21_TRIG_RH_NOHOLD_ADDR (0xF5)
+#define SHT21_USER_REG_ADDR       (0xE6)
+#define SHT21_SOFT_RESET_ADDR     (0xFE)
+
+typedef union
+{
+  uint8_t bytes[2];
+  uint16_t data;
+} SHT21_ADC_CONV_t;
+
+typedef union 
+{
+  uint8_t reg;
+  struct
+  {
+    bool T_Resolution : 1;
+    bool Disable_OTP_Reload : 1;
+    uint8_t Reserved : 3;
+    bool End_of_Battery : 1;
+    bool RH_Resolution : 1;
+  };
+} SHT21_USER_REG_t;
